8faaa9496773586fac1a38fc27c494f1bdbbbd3e
thorntonmk <morgan.thornton@bison.howard.edu>
2025-01-30 12:23:11 -0600

    Feat: added edit, delete button

--- app/(tabs)/medication.tsx
+++ app/(tabs)/medication.tsx
@@ -2,15 +2,16 @@ import React, { useState, useEffect, useRef } from 'react';
 
 import {
   Image,
+  ListRenderItemInfo,
   SafeAreaView,
   StyleSheet,
   TouchableOpacity
 } from 'react-native';
-import Animated from 'react-native-reanimated';
 
+import { RowMap, SwipeListView } from 'react-native-swipe-list-view';
 import { getMedicationList } from '@/services/medication';
 import { IMedication, TResponse } from '@/@types';
-import { BellIcon, FlyIcon, PillIcon, SettingIcon, StockIcon } from '@/utils/assets';
+import { BellIcon, DeleteIcon, EditIcon, FlyIcon, PillIcon, SettingIcon, StockIcon } from '@/utils/assets';
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
 import { useTranslation } from 'react-i18next';
@@ -46,76 +47,102 @@ export default function HomeScreen() {
     }
   }
 
+  const handleEditRow = (rowMap: RowMap<IMedication>, id: number): void => {
+
+  }
+
+  const handleDeleteRow = (rowMap: RowMap<IMedication>, id: number): void => {
+
+  }
+
+  const handleOpenedRow = (): void => {
+
+  }
+
+  const renderItem = (data: ListRenderItemInfo<IMedication>) => (
+    <ThemedView style={styles.itemWrapper}>
+      <Image source={{ uri: data.item.image }} width={60} height={60}/>
+      <ThemedView style={styles.infoWrapper}>
+        <ThemedView style={{ flexGrow: 1 }}>
+          <ThemedText style={styles.itemTitle}>{data.item.name}</ThemedText>
+          <ThemedView style={styles.itemTextWrapper}>
+            <PillIcon />
+            <ThemedText style={styles.normalText}>{data.item.dosage}</ThemedText>
+            <ThemedText>{'•'}</ThemedText>
+            <ThemedText style={styles.normalText}>{data.item.frequency}</ThemedText>
+          </ThemedView>
+          <ThemedView style={styles.itemTextWrapper}>
+            <StockIcon color={getColorByLevel(data.item.stock)} />
+            <ThemedText
+              style={[styles.normalText, { color: getColorByLevel(data.item.stock) }]}
+            >
+              {`${data.item.stock} Tablets`}
+            </ThemedText>
+          </ThemedView>
+        </ThemedView>
+        <ThemedView style={{ rowGap: 5 }}>
+          <ThemedView style={{ flexDirection: 'row', columnGap: 5 }}>
+            <BellIcon />
+            <FlyIcon />
+          </ThemedView>
+          <ThemedView>
+            <TouchableOpacity onPress={() => {}}>
+              <SettingIcon />
+            </TouchableOpacity>
+          </ThemedView>
+        </ThemedView>
+      </ThemedView>
+    </ThemedView>
+  );
+
+  const renderHiddenItem = (data: ListRenderItemInfo<IMedication>, rowMap: RowMap<IMedication>) => (
+    <ThemedView style={styles.rowBack}>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnLeft]}
+        onPress={() => handleEditRow(rowMap, data.item.id)}
+      >
+        <EditIcon />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.backRightBtn, styles.backRightBtnRight]}
+        onPress={() => handleDeleteRow(rowMap, data.item.id)}
+      >
+        <DeleteIcon />
+      </TouchableOpacity>
+    </ThemedView>
+  );
+
   return (
     <SafeAreaView style={styles.mainWrapper}>
       <ThemedView style={styles.listHeader}>
         <ThemedText
-          style={[
-            styles.listHeaderText,
-            { width: '20%' }
-          ]}
+          style={[styles.listHeaderText, { width: '20%' }]}
         >
           {t('medication_manage.item')}
         </ThemedText>
         <ThemedText
-          style={[
-            styles.listHeaderText,
-            { width: '40%' }
-          ]}
+          style={[styles.listHeaderText, { width: '40%' }]}
         >
           {t('medication_manage.description')}
         </ThemedText>
         <ThemedText
-          style={[
-            styles.listHeaderText,
-            { width: '40%' }
-          ]}
+          style={[styles.listHeaderText, { width: '40%' }]}
         >
           {t('medication_manage.refill_reminder')}
         </ThemedText>
       </ThemedView>
-      <Animated.ScrollView style={styles.mainWrapper}>
-        {medicationList.map((medication: IMedication, index: number) => 
-          <ThemedView key={index} style={styles.itemWrapper}>
-            <Image source={{ uri: medication.image }} width={60} height={60}/>
-            <ThemedView style={styles.infoWrapper}>
-              <ThemedView style={{ flexGrow: 1 }}>
-                <ThemedText style={styles.itemTitle}>{medication.name}</ThemedText>
-                <ThemedView style={styles.itemTextWrapper}>
-                  <PillIcon />
-                  <ThemedText style={styles.normalText}>{medication.dosage}</ThemedText>
-                  <ThemedText>{'•'}</ThemedText>
-                  <ThemedText style={styles.normalText}>{medication.frequency}</ThemedText>
-                </ThemedView>
-                <ThemedView style={styles.itemTextWrapper}>
-                  <StockIcon color={getColorByLevel(medication.stock)} />
-                  <ThemedText
-                    style={[
-                      styles.normalText,
-                      {
-                        color: getColorByLevel(medication.stock)
-                      }
-                    ]}
-                  >
-                    {`${medication.stock} Tablets`}
-                  </ThemedText>
-                </ThemedView>
-              </ThemedView>
-              <ThemedView style={{ rowGap: 5 }}>
-                <ThemedView style={{ flexDirection: 'row', columnGap: 5 }}>
-                  <BellIcon />
-                  <FlyIcon />
-                </ThemedView>
-                <ThemedView>
-                  <TouchableOpacity onPress={() => {}}>
-                    <SettingIcon />
-                  </TouchableOpacity>
-                </ThemedView>
-              </ThemedView>
-            </ThemedView>
-          </ThemedView>
-        )}
-      </Animated.ScrollView>
+      <SwipeListView
+        style={styles.mainWrapper}
+        data={medicationList}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={0}
+        rightOpenValue={-150}
+        previewRowKey={'0'}
+        previewOpenValue={0}
+        previewOpenDelay={3000}
+        onRowDidOpen={handleOpenedRow}
+      />
     </SafeAreaView>
   );
 }
@@ -162,5 +189,32 @@ const styles = StyleSheet.create({
   normalText: {
     fontSize: 14,
     fontWeight: 400,
-  }
+  },
+  rowBack: {
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    width: 75,
+  },
+  backRightBtnLeft: {
+    backgroundColor: '#07d22d',
+    right: 75,
+  },
+  backRightBtnRight: {
+    backgroundColor: '#fb4420',
+    right: 0,
+  },
+  backTextWhite: {
+    color: '#fff',
+  },
 });
