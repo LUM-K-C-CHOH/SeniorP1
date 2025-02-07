c2248b3c953b4ba3a7e7bb2d1d71df62f4a31265
thorntonmk <morgan.thornton@bison.howard.edu>
2025-02-07 15:08:00 -0600

    Feat: added calendar component

---
+++ components/Calendar.tsx
@@ -0,0 +1,18 @@
+/**
+ * Calendar.tsx
+ * Author Morgan Thornton
+ * Created By Thornton at 2025.02.07
+ */
+import React from 'react';
+
+type TCalendarProps = {
+  date: string
+};
+
+const Calendar = ({ date }: TCalendarProps): JSX.Element => {
+  return (
+    <></>
+  );
+}
+
+export default Calendar;
